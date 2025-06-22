b764191d5f215d5e5c2e6e972922981301dde197
VsalCode <vsal.ilham@gmail.com>
2025-06-22 19:38:12 +0700

    feat: add query sql for authflow

--- auth.sql
+++ auth.sql
@@ -1,26 +1,18 @@
-DROP TABLE IF EXISTS login_session;
-DROP TABLE IF EXISTS "user";
-
-CREATE TABLE "user" (
-    id_user SERIAL PRIMARY KEY,
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
     fullname VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
-    is_verified BOOLEAN NOT NULL DEFAULT FALSE,
-    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
+    is_verified BOOLEAN DEFAULT FALSE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE login (
-    id_login SERIAL PRIMARY KEY,
-    token VARCHAR(255) NOT NULL,
-    status_login BOOLEAN NOT NULL DEFAULT TRUE,
-    login_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    expiry_time TIMESTAMP,
-    id_user INTEGER NOT NULL,
-    CONSTRAINT fk_user FOREIGN KEY (id_user) REFERENCES "user"(id_user) ON DELETE CASCADE
+CREATE TABLE sessions (
+    id SERIAL PRIMARY KEY,
+    token VARCHAR(45) UNIQUE NOT NULL,
+    is_active BOOLEAN DEFAULT TRUE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    expiry_time TIMESTAMP NOT NULL,
+    user_id VARCHAR(36) REFERENCES users(id) ON DELETE CASCADE,
 );
-
-CREATE INDEX idx_user_email ON "user"(email);
-CREATE INDEX idx_login_id_user ON login(id_user);
-CREATE INDEX idx_login_token ON
